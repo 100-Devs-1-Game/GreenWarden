6370fca116bcd3447600f4f2277bfa3dc6e9a77f
hollowMoon <122719873+shallowfrost@users.noreply.github.com>
2025-09-10 11:46:46 -0500

    updated player.gd

--- src/assets/Modules/Player2DFloat/player.gd
+++ src/assets/Modules/Player2DFloat/player.gd
@@ -16,11 +16,11 @@ func _ready() -> void:
 			event.physical_keycode = key
 			InputMap.action_add_event(action, event)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	var wasd = Input.get_vector("left", "right", "up", "down")
 	if Input.get_connected_joypads().size() > 0:
 		if wasd.length() == 0: 
 			wasd = Vector2(Input.get_joy_axis(0,0), Input.get_joy_axis(0,1))
 		if wasd.length() < stickDriftComp: wasd = Vector2.ZERO
 	$Sprite2D.region_rect = Rect2i(round(wasd * 11 + Vector2(11, 11)) * 32, Vector2(32, 32))
-	position += wasd * scale * 4
+	position += wasd * scale * delta * 350
