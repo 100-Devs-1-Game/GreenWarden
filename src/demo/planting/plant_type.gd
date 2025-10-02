a57dd3d877075c09a748a7462024736c89f54032
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-02 03:42:24 +0200

    Random harvest amount

--- src/demo/planting/plant_type.gd
+++ src/demo/planting/plant_type.gd
@@ -6,3 +6,5 @@ extends Resource
 @export var num_growth_stages: int= 3
 @export var growth_time_per_stage: float= 10.0
 @export var harvest_item: Item
+@export var harvest_min: int= 1
+@export var harvest_max: int= 2
